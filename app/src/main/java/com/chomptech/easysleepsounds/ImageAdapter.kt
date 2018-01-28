ca19e0dff65f0a0352d56f3ab117b00eaf7a1e84
Christopher Dalporto <christopher.dalporto@outlook.com>
2018-01-28 04:03:29 -0800

    V 1.0 initial commit.

---
+++ app/src/main/java/com/chomptech/easysleepsounds/ImageAdapter.kt
@@ -0,0 +1,51 @@
+package com.chomptech.easysleepsounds
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.BaseAdapter
+import android.widget.ImageView
+
+
+/**
+ * Created by wp7ch on 1/24/2018.
+ */
+class ImageAdapter(private val mContext: Context) : BaseAdapter() {
+
+
+
+    // references to our images
+    private val mThumbIds = arrayOf<Int>(R.drawable.rain, R.drawable.thunder, R.drawable.stop)
+
+    override fun getCount(): Int {
+        return mThumbIds.size
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val imageView: ImageView
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = ImageView(mContext)
+            imageView.setLayoutParams(AbsListView.LayoutParams(280, 280))
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP)
+            imageView.setPadding(8, 8, 8, 8)
+        } else {
+            imageView = (convertView as ImageView?)!!
+        }
+
+        imageView.setImageResource(mThumbIds[position])
+        return imageView
+    }
+
+
+}
---
+++ app/src/main/java/com/chomptech/easysleepsounds/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.chomptech.easysleepsounds
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.GridView
+import android.media.MediaPlayer
+import android.os.PowerManager
+import android.os.CountDownTimer
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdView
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    var mediaPlayer = MediaPlayer()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this, "ca-app-pub-9999083812241050~7895146987");
+
+        val mAdView = findViewById<View>(R.id.adView) as AdView
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        val gridview = findViewById<View>(R.id.gridview) as GridView
+        gridview.adapter = ImageAdapter(this)
+
+        gridview.onItemClickListener = object : OnItemClickListener {
+            override fun onItemClick(parent: AdapterView<*>, v: View,
+                                     position: Int, id: Long) {
+                startAudio(position)
+            }
+        }
+    }
+    fun startAudio(num : Int) {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+
+        when (num) {
+            0 -> { mediaPlayer = MediaPlayer.create(applicationContext, R.raw.rain)
+            play()}
+            1 -> {mediaPlayer = MediaPlayer.create(applicationContext, R.raw.thunder)
+            play()}
+            3 -> if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+                mediaPlayer = MediaPlayer()
+            }
+            else -> { // Note the block
+
+            }
+        }
+    }
+
+    fun play() {
+        //val mediaPlayer = MediaPlayer.create(applicationContext, R.raw.rain)
+        mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mediaPlayer.setLooping(true)
+        mediaPlayer.start() // no need to call prepare(); create() does that for you
+
+        Toast.makeText(applicationContext, "Sound will play for one hour or until stopped manually with the in-app STOP button",
+                Toast.LENGTH_LONG).show();
+
+
+        object : CountDownTimer(3600000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.stop()
+                    mediaPlayer.release()
+                    mediaPlayer = MediaPlayer()  //CHECK THE BEHAVIOR LATER WHAT HAPPENS ATTEMPTING
+                    // PLAYBACK AFTER TIMER RUNS OUT, WIHTOUT THIS LINE OF CODE. THIS IS KIND OF DIRTY BECAUSE NOT RELEASED ANYMORE
+                }
+            }
+        }.start()
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(applicationContext, "Back button pressed, app exiting.",
+                Toast.LENGTH_SHORT).show();
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+        mediaPlayer.release()
+        super.onBackPressed()
+    }
+
+    }
+
